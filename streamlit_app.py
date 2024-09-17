e7d4f5d5a6acf310d952079769fb4dc9505faa4f
VsinghOffice <Vsingh@visvero.com>
2024-09-17 09:59:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,6 @@ if ingredients_list and max_selection(ingredients_list):
         st.stop()
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
