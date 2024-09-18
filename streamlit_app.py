b3160f7034c2ebf1de9cde74f46456d45b5ac74d
VsinghOffice <Vsingh@visvero.com>
2024-09-18 08:30:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,44 @@
+import streamlit as st
+import requests
+from snowflake.snowpark.functions import col
+ 
+# Write directly to the app
+st.title("My Parents New Healthy Diner")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!.
+    """
+)
+ 
+name_on_order = st.text_input('Name on smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+ 
+# Get session and fetch data
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+ 
+# Ingredient selection
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
+ 
+if ingredients_list:
+    ingredients_string = ' '.join(ingredients_list)
+    st.write("Your selected ingredients:", ingredients_string)
+ 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+ 
+    # SQL insert statement
+    my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
+    # Button with a unique key to avoid the DuplicateWidgetID issue
+    time_to_insert = st.button('Submit Order', key='submit_order')
+ 
+    # Insert when the button is clicked
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 # import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 # from snowflake.snowpark.functions import col, when_matched
