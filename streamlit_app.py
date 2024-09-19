83978a1b29a1e639d646825e3eb998d63b7dbb9d
VsinghOffice <Vsingh@visvero.com>
2024-09-19 06:28:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,12 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Ingredient selection
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)
+    ingredients_string = ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+    
     st.write("Your selected ingredients:", ingredients_string)
     # SQL insert statement
     my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
@@ -27,9 +32,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
 
 
 # import streamlit as st
