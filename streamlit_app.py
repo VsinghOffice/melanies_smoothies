fdcbe9a7e420f1fe0a1ff87e1ced2bcf6b3f7829
VsinghOffice <Vsingh@visvero.com>
2024-09-19 06:18:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,68 +95,34 @@
 # st.dataframe(my_dataframe[['NAME_ON_ORDER', 'INGREDIENTS']])
 
 
-# Import python packages
-
 import streamlit as st
-
 from snowflake.snowpark.functions import col
- 
 # Write directly to the app
-
 st.title("My Parents New Healthy Diner")
-
 st.write(
+    """Breakfast Menu
 
     """
-
-    """
-
 )
- 
-name_on_order = st.text_input("Name on smoothie")
-
-st.write("The name on the smoothie will be ", name_on_order)
+name_on_order = st.text_input('Name on smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+# Get session and fetch data
 cnx = st.connection("snowflake")
-session = get_active_session()
-
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
-st.dataframe(data=my_dataframe, use_container_width=True)
- 
-ingredients_list = st.multiselect(
-
-'Choose up to 5 ingredients;'
-
-, my_dataframe
-
-)
- 
+# Ingredient selection
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
 if ingredients_list:
-
-     ingredients_string = ''
- 
-     for fruit_chosen in ingredients_list:
-
-         ingredients_string += fruit_chosen + ''
- 
-     #st.write(ingredients_string)
-
-     time_to_insert = st.button('Submit Order')
- 
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
- 
-     st.write(my_insert_stmt)
-
-     st.stop()
- 
-     if time_to_insert:
-
+    ingredients_string = ' '.join(ingredients_list)
+    st.write("Your selected ingredients:", ingredients_string)
+    # SQL insert statement
+    my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
+    # Button with a unique key to avoid the DuplicateWidgetID issue
+    time_to_insert = st.button('Submit Order', key='submit_order')
+    # Insert when the button is clicked
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
-
         st.success('Your Smoothie is ordered!', icon="✅")
-
  
 
 
