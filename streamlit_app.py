26e0c42f6cccc99416f19b1b041702095eb0b836
VsinghOffice <Vsingh@visvero.com>
2024-09-19 06:30:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, m
 if ingredients_list:
     ingredients_string = ' '
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
